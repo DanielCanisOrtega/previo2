e01489c4c9a70f0f998b417d597969b0c1b8c504
Daniel <danielstevenco@ufps.edu.co>
2024-11-15 07:03:05 -0500

    se agregan los modelos proyecto y tarea proyecto

--- apis/models.py
+++ apis/models.py
@@ -29,3 +29,29 @@ class DetalleOrden(models.Model):
 
     def __str__(self):
         return f"{self.producto} - {self.cantidad} unidades"
+    
+class Proyecto(models.Model):
+    ESTADO_CHOICES = [
+        ('Pendiente', 'Pendiente'),
+        ('En Progreso', 'En Progreso'),
+        ('Completado', 'Completado'),
+    ]
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField()
+    estado = models.CharField(max_length=20, choices=ESTADO_CHOICES, default='Pendiente')
+
+    def __str__(self):
+        return self.nombre
+
+
+class TareaProyecto(models.Model):
+    proyecto = models.ForeignKey(Proyecto, related_name='tareas', on_delete=models.CASCADE)
+    nombre_tarea = models.CharField(max_length=100)
+    responsable = models.CharField(max_length=100)
+    fecha_limite = models.DateField()
+    completado = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Tarea: {self.nombre_tarea} - Proyecto: {self.proyecto.nombre}"
