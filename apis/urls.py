2c671d044aede983d38fb74dcc98d2f810d4808b
Daniel <danielstevenco@ufps.edu.co>
2024-11-15 07:12:59 -0500

    se añade la vista y el enrutamiento de los proyectos

--- apis/urls.py
+++ apis/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import OrdenViewSet
+from .views import OrdenViewSet, ProyectoViewSet
 
 router = DefaultRouter()
 router.register(r'ordenes', OrdenViewSet, basename='orden')
+router.register(r'proyectos', ProyectoViewSet, basename='proyecto')
+
 
 urlpatterns = [
     path('', include(router.urls)),
--- apis/views.py
+++ apis/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
-from .models import Orden
-from .serializers import OrdenSerializer
+from .models import Orden, Proyecto
+from .serializers import OrdenSerializer, ProyectoSerializer
 
 # Create your views here.
 
@@ -11,7 +11,7 @@ from .serializers import OrdenSerializer
 class OrdenViewSet(viewsets.ModelViewSet):
     queryset = Orden.objects.all()
     serializer_class = OrdenSerializer
-
+    
     @action(detail=True, methods=['patch'], url_path='cambiar-estado')
     def cambiar_estado(self, request, pk=None):
         orden = self.get_object()
@@ -21,5 +21,20 @@ class OrdenViewSet(viewsets.ModelViewSet):
         orden.estado = nuevo_estado
         orden.save()
         return Response({'message': f'Estado cambiado a {nuevo_estado}'})
+    
+class ProyectoViewSet(viewsets.ModelViewSet):
+    queryset = Proyecto.objects.all()
+    serializer_class = ProyectoSerializer
+
+    @action(detail=True, methods=['get'], url_path='progreso')
+    def progreso_proyecto(self, request, pk=None):
+        proyecto = self.get_object()
+        total_tareas = proyecto.tareas.count()
+        if total_tareas == 0:
+            progreso = 0
+        else:
+            tareas_completadas = proyecto.tareas.filter(completado=True).count()
+            progreso = round((tareas_completadas / total_tareas) * 100, 2)
+        return Response({'progreso': f'{progreso}% de tareas completadas'})
 
 
