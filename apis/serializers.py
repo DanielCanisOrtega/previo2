b48d199900c083073b81fea95e7a6448340c0bf8
Daniel <danielstevenco@ufps.edu.co>
2024-11-15 07:06:25 -0500

    se agregan los serializadores de proyecto y tarea proyecto

--- apis/serializers.py
+++ apis/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Orden, DetalleOrden
+from .models import Orden, DetalleOrden, Proyecto, TareaProyecto
 
 class DetalleOrdenSerializer(serializers.ModelSerializer):
     subtotal = serializers.SerializerMethodField()
@@ -18,3 +18,24 @@ class OrdenSerializer(serializers.ModelSerializer):
     class Meta:
         model = Orden
         fields = ['id', 'numero_orden', 'fecha', 'estado', 'total', 'detalles']
+
+class TareaProyectoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TareaProyecto
+        fields = ['id', 'nombre_tarea', 'responsable', 'fecha_limite', 'completado']
+
+
+class ProyectoSerializer(serializers.ModelSerializer):
+    tareas = TareaProyectoSerializer(many=True, read_only=True)
+    progreso = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Proyecto
+        fields = ['id', 'nombre', 'descripcion', 'fecha_inicio', 'fecha_fin', 'estado', 'tareas', 'progreso']
+
+    def get_progreso(self, obj):
+        total_tareas = obj.tareas.count()
+        if total_tareas == 0:
+            return 0
+        tareas_completadas = obj.tareas.filter(completado=True).count()
+        return round((tareas_completadas / total_tareas) * 100, 2)
